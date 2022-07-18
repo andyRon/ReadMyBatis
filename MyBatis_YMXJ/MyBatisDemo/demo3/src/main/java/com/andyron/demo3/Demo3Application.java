69786db3aefabb790a715d9230531e5c0793dc77
Andy Ron <randy.njfu@gmail.com>
2022-07-18 23:14:04 +0800

    《通用源码阅读指导书：MyBatis源码详解》第一部分

---
+++ MyBatis_YMXJ/MyBatisDemo/demo3/src/main/java/com/andyron/demo3/Demo3Application.java
@@ -0,0 +1,40 @@
+package com.andyron.demo3;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@SpringBootApplication
+public class Demo3Application {
+
+    public static void main(String[] args) {
+        // 第一阶段：mybatis初始化
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 第二阶段：数据读写阶段
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            List<User> userList = userMapper.getUserList();
+            for (User user : userList) {
+                System.out.println(user);
+            }
+        }
+
+//        SpringApplication.run(Demo3Application.class, args);
+    }
+
+}
---
+++ MyBatis_YMXJ/MyBatisDemo/demo3/src/main/java/com/andyron/demo3/UserMapper.java
@@ -0,0 +1,14 @@
+package com.andyron.demo3;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    List<User> getUserList();
+
+    User getUserById(Long id);
+}
